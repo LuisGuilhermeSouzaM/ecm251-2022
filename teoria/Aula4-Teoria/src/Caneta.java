dc8281f295ee34a2a7ef4121323718d405eb7f94
LuisGuilhermeMunhoz <mnomeluisguilherme@yahoo.com>
2022-03-09 11:09:31 -0300

    criação de classes com java

--- teoria/Aula4-Teoria/src/Caneta.java
+++ teoria/Aula4-Teoria/src/Caneta.java
@@ -1,3 +1,13 @@
 public class Caneta {
-    
+    //Características da caneta - seus atributos
+    String modelo;
+    String cor;
+    double ponta;
+    int carga;
+
+    //Comportamoentos da caneta - seus Métodos
+    void escrever(String texto){
+        System.out.println(texto);
+    }
+
 }
