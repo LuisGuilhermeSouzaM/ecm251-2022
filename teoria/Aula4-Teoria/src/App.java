e6e96e7832f4798fe742660309decfcc089abb88
LuisGuilhermeSouzaM <81170691+LuisGuilhermeSouzaM@users.noreply.github.com>
2022-03-10 11:00:13 -0300

    upgrade da caneta

--- teoria/Aula4-Teoria/src/App.java
+++ teoria/Aula4-Teoria/src/App.java
@@ -1,14 +1,14 @@
 public class App {
     public static void main(String[] args){
-        //Declara e Instancia um objeto caneta
+        //Declara e Instancia um objeto do tipo caneta
         Caneta c1 = new Caneta();
-        c1.modelo = "BIC";
-        c1.cor = "azul";
-        c1.carga = 100;
-        c1.ponta = 1.0;
-        System.out.println("Minha caneta:"+c1.modelo);
-        System.out.println("Minha caneta:"+c1.cor);
-        System.out.println("Minha caneta:"+c1.carga);
-        System.out.println("Minha caneta:"+c1.ponta);
+        c1.iniciarCaneta("azul", "BIC", 1.0);
+        Caneta c2 = new Caneta();
+        c2.iniciarCaneta("vermelha", "Stabillo", 0.4);
+
+        c1.escrever("Olá Mundo, o Batman novo é um bom filme");
+        c2.escrever("Missão Impossível pode mesmo ser o homem de ferro?");
+        c2.escrever("Missão Impossível pode mesmo ser o homem de ferro?");
+        c2.escrever("Missão Impossível pode mesmo ser o homem de ferro?");
     }
 }
--- teoria/Aula4-Teoria/src/Caneta.java
+++ teoria/Aula4-Teoria/src/Caneta.java
@@ -1,13 +1,36 @@
 public class Caneta {
-    //Características da caneta - seus atributos
+    // Características da caneta - seus atributos
     String modelo;
     String cor;
     double ponta;
     int carga;
+    final int CARGA_INICIAL = 100;
 
-    //Comportamoentos da caneta - seus Métodos
-    void escrever(String texto){
-        System.out.println(texto);
+    void iniciarCaneta(String cor, String modelo, double ponta) {
+        this.cor = cor;
+        this.modelo = modelo;
+        this.ponta = ponta;
+        this.carga = CARGA_INICIAL;
     }
 
+    // Comportamoentos da caneta - seus Métodos
+    void escrever(String texto) {
+        for(int i =0; i < texto.length(); i++){
+            if(this.carga > 0){
+                System.out.print(texto.charAt(i));
+                this.carga--;
+            }else{
+                System.out.println("\nCaneta sem carga");
+                break;
+            }
+        }
+        System.out.println();
+    }
+
+    String mostrarDados() {
+        return "Modelo: " + this.modelo +
+                " - Cor: " + this.cor +
+                " - Ponta: " + this.ponta +
+                " - Carga: " + this.carga;
+    }
 }
