ba32b93227894f3dc3fc7a06a3ea8fb0089604ac
LuisGuilhermeMunhoz <mnomeluisguilherme@yahoo.com>
2022-03-12 16:03:11 -0300

    caneta tampada

--- teoria/Aula4-Teoria/src/App.java
+++ teoria/Aula4-Teoria/src/App.java
@@ -2,13 +2,14 @@ public class App {
     public static void main(String[] args){
         //Declara e Instancia um objeto do tipo caneta
         Caneta c1 = new Caneta();
-        c1.iniciarCaneta("azul", "BIC", 1.0);
+        c1.iniciarCaneta("azul", "BIC", 1.0, false);
         Caneta c2 = new Caneta();
-        c2.iniciarCaneta("vermelha", "Stabillo", 0.4);
+        c2.iniciarCaneta("vermelha", "Stabillo", 0.4, true);
 
         c1.escrever("Olá Mundo, o Batman novo é um bom filme");
         c2.escrever("Missão Impossível pode mesmo ser o homem de ferro?");
-        c2.escrever("Missão Impossível pode mesmo ser o homem de ferro?");
+        c2.destamparCaneta();
+        c2.escrever("bom dia");
         c2.escrever("Missão Impossível pode mesmo ser o homem de ferro?");
     }
 }
--- teoria/Aula4-Teoria/src/Caneta.java
+++ teoria/Aula4-Teoria/src/Caneta.java
@@ -3,34 +3,46 @@ public class Caneta {
     String modelo;
     String cor;
     double ponta;
+    boolean tampada;
     int carga;
     final int CARGA_INICIAL = 100;
 
-    void iniciarCaneta(String cor, String modelo, double ponta) {
+    void iniciarCaneta(String cor, String modelo, double ponta, boolean tampada) {
         this.cor = cor;
         this.modelo = modelo;
         this.ponta = ponta;
         this.carga = CARGA_INICIAL;
+        this.tampada = tampada;
     }
 
     // Comportamoentos da caneta - seus Métodos
     void escrever(String texto) {
-        for(int i =0; i < texto.length(); i++){
-            if(this.carga > 0){
-                System.out.print(texto.charAt(i));
-                this.carga--;
-            }else{
-                System.out.println("\nCaneta sem carga");
-                break;
+        if(this.tampada == false){
+            for(int i =0; i < texto.length(); i++){
+                if(this.carga > 0){
+                    System.out.print(texto.charAt(i));
+                    this.carga--;
+                }else{
+                    System.out.println("\nCaneta sem carga");
+                    break;
+                }
             }
+            System.out.println();
+        }else{
+            System.out.println("Caneta tampada");
         }
-        System.out.println();
+            
     }
 
     String mostrarDados() {
         return "Modelo: " + this.modelo +
                 " - Cor: " + this.cor +
                 " - Ponta: " + this.ponta +
-                " - Carga: " + this.carga;
+                " - Carga: " + this.carga +
+                "- Tampada: " + this.tampada;
+    }
+
+    boolean destamparCaneta(){
+        return this.tampada = false;
     }
 }
