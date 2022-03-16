f22227d1d6c4f87788de9f0f6016f56171fd26ea
LuisGuilhermeMunhoz <mnomeluisguilherme@yahoo.com>
2022-03-16 11:03:45 -0300

    atualizações do app do banco

--- teoria/Aula05-Teoria/exe-poo/src/App.java
+++ teoria/Aula05-Teoria/exe-poo/src/App.java
@@ -1,5 +1,28 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception 
+    {
+        //Declara um objeto do tipo conta
+        Conta minhaConta;
+        //Instanciar um objeto do tipo conta
+        minhaConta = new Conta();
+        //Declarou e instanciou um objeto do tipo conta
+        Conta outraConta = new Conta();
+
+        minhaConta.saldo = 200.0;
+        minhaConta.numero = 1234;
+        outraConta.saldo = 150.00;
+        outraConta.numero = 5678;
+
+        System.out.println("Saldo na minha conta: ");
+        minhaConta.visualizarSaldo();
+        System.out.println("Saldo na outra conta: ");
+        outraConta.visualizarSaldo();
+
+        minhaConta.transferirDinheiro(-100, outraConta);
+
+        System.out.println("Saldo na minha conta: ");
+        minhaConta.visualizarSaldo();
+        System.out.println("Saldo na outra conta: ");
+        outraConta.visualizarSaldo();
     }
 }
