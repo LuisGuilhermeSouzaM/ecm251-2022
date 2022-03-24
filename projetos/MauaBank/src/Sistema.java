586d158c203a11f2f96e39bcd6a1bdf1e4d72b3c
LuisGuilhermeSouzaM <81170691+LuisGuilhermeSouzaM@users.noreply.github.com>
2022-03-24 11:01:39 -0300

    Criação do sistema

---
+++ projetos/MauaBank/src/Sistema.java
@@ -0,0 +1,30 @@
+import java.time.LocalDate;
+
+import javax.xml.crypto.Data;
+
+public class Sistema {
+    public void run(){
+        Cliente cliente = new Cliente("Midoria", "123456", "allmigth_wannabe@gmail.com");
+        Conta conta  = new Conta(cliente, 1234);
+
+        System.out.println(conta);
+
+        Titulo steam = new Titulo(200,
+        LocalDate.of(2022, 03, 30), 5);
+
+        conta.depositar(300);
+    }
+
+    boolean pagarTitulo(Titulo titulo, 
+    Conta conta){
+        double valorPagar;
+        LocalDate dataTitulo = titulo.getData();
+        LocalDate hoje = LocalDate.now();
+        if(dataTitulo.compareTo(hoje) > 0){
+            valorPagar = titulo.getValor();
+        } else{
+            //TODO: terminar a implementação
+        }
+        return true;
+    }
+}
