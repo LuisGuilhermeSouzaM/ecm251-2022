dea6deefcf281291ebf9341d5d8f2a0ba1f72b95
LuisGuilherme-SM <81170691+LuisGuilhermeSouzaM@users.noreply.github.com>
2022-03-31 10:26:36 -0300

    a

---
+++ SimuladoT1/SimuladoAtividade1/src/Sistema.java
@@ -0,0 +1,12 @@
+public class Sistema {
+    public void run(){
+        
+    }
+
+    boolean pagarQRCode(String qRCode){
+        String[] dados = qRCode.split(";");
+        
+        if (dados[2] < )
+        return true;
+    }
+}
