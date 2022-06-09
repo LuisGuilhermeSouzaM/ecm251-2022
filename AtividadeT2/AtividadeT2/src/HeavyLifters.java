db382da27cc519dcb4efcea9ef22641a9f1fe2b9
LUIS GUILHERME DE SOUZA MUNHOZ <20.01937-8@maua.br>
2022-06-09 10:09:12 -0300

    HeavyLifters

---
+++ AtividadeT2/AtividadeT2/src/HeavyLifters.java
@@ -0,0 +1,15 @@
+public class HeavyLifters extends Membro{
+
+    public HeavyLifters(String nome, String email, EnumFuncao funcao, EnumHora hora) {
+        super(nome, email, funcao, hora);
+    }
+
+    @Override
+    public void postarMensagem() {
+        if(this.getHora() == EnumHora.TRABALHO)
+            System.out.println("Podem contar conosco!");
+        else if(this.getHora() == EnumHora.EXTRAS)
+            System.out.println("N00b_qu3_n_Se_r3pita.bat");
+    }
+    
+}
