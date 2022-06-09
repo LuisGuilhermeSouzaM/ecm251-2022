9328eba8726c52e6202a48a350df18710e5a0ef3
LUIS GUILHERME DE SOUZA MUNHOZ <20.01937-8@maua.br>
2022-06-09 10:07:19 -0300

    script guys

--- AtividadeT2/AtividadeT2/src/ScriptGuys.java
+++ AtividadeT2/AtividadeT2/src/ScriptGuys.java
@@ -1,3 +1,15 @@
 public class ScriptGuys extends Membro{
+
+    public ScriptGuys(String nome, String email, EnumFuncao funcao, EnumHora hora) {
+        super(nome, email, funcao, hora);
+    }
+
+    @Override
+    public void postarMensagem() {
+        if(this.getHora() == EnumHora.TRABALHO)
+            System.out.println("Prazer em ajudar novos amigos de código!");
+        else if(this.getHora() == EnumHora.EXTRAS)
+            System.out.println("QU3Ro_S3us_r3curs0s.py");
+    }
     
 }
